05096fc985d678a0250dda767224432a86679300
AKRAUSE2017 <akrause2017@fau.edu>
2017-07-26 13:56:37 -0400

    riddleCheck

--- scripts/riddles.js
+++ scripts/riddles.js
@@ -1,10 +1,23 @@
 var check = 0;
 
 function checkRiddles(){
-  if(($("#txtQ1").val() == "PUFFER FISH")){
+    if(($("#txtQ1").val() == "PUFFER FISH")){
     check=1;
   } if (($("#txtQ2").val() == "ROYAL GRAMMA")){
     check=2;
     console.log(check);
-  } if (($("#txtQ3").val() == ""))
+  } if (($("#txtQ3").val() == "HORSESHOE CRAB")){
+    check=3;
+    console.log(check);
+  } if (($("#txtQ4").val() == "LOOKDOWN FISH")){
+    check=4;
+    console.log(check);
+  } if (($("#txtQ5").val() == "BLIND EYE CAVE FISH")){
+    check=5;
+    console.log(check);
+  } if (($("#txtQ6").val() == "BRAIN CORAL")){
+    check=6;
+    console.log(check);
+  }
+  alert("You answered "+check+"/6 questions correctly!");
 }
