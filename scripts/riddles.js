076d5b723f084d621ae03b6dddc6be240c84a169
AKRAUSE2017 <akrause2017@fau.edu>
2017-07-26 12:29:22 -0400

    riddles

--- scripts/riddles.js
+++ scripts/riddles.js
@@ -1,3 +1,10 @@
+var check = 0;
+
 function checkRiddles(){
-  
+  if(($("#txtQ1").val() == "PUFFER FISH")){
+    check=1;
+  } if (($("#txtQ2").val() == "ROYAL GRAMMA")){
+    check=2;
+    console.log(check);
+  }
 }
