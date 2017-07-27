564e1daed651bb9476310815eb46250082c13580
AKRAUSE2017 <akrause2017@fau.edu>
2017-07-27 14:47:42 -0400

    inventory pretty much done

--- scripts/riddles.js
+++ scripts/riddles.js
@@ -1,7 +1,7 @@
 var check = 0;
 
 function checkRiddles(){
-    if(($("#txtQ1").val() == "PUFFER FISH")){
+    if(($("#txtQ1").val() == "DOGFISH SHARK")){
     check=1;
   } if (($("#txtQ2").val() == "ROYAL GRAMMA")){
     check=2;
