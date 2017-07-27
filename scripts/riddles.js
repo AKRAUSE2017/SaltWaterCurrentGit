b57439c9b90fa45ce17823f1e2acaae74e2aa25b
AKRAUSE2017 <akrause2017@fau.edu>
2017-07-27 16:48:52 -0400

    Pretty much the final

--- scripts/riddles.js
+++ scripts/riddles.js
@@ -1,21 +1,21 @@
 var check = 0;
 
 function checkRiddles(){
-    if(($("#txtQ1").val() == "DOGFISH SHARK")){
+    if(($("#txtQ1").val() == "DOGFISH SHARK" || "dogfish shark" || "Dogfish Shark" || "Dogfish shark")){
     check=1;
-  } if (($("#txtQ2").val() == "ROYAL GRAMMA")){
+  } if (($("#txtQ2").val() == "ROYAL GRAMMA" || "royal gramma" || "Royal Gramma" || "Royal gramma")){
     check=2;
     console.log(check);
-  } if (($("#txtQ3").val() == "HORSESHOE CRAB")){
+  } if (($("#txtQ3").val() == "HORSESHOE CRAB" || "horseshoe crab" || "Horseshoe Crab" || "Horseshoe crab")){
     check=3;
     console.log(check);
-  } if (($("#txtQ4").val() == "LOOKDOWN FISH")){
+  } if (($("#txtQ4").val() == "LOOKDOWN FISH" || "lookdown fish" || "LookDown Fish" || "LookDown fish")){
     check=4;
     console.log(check);
-  } if (($("#txtQ5").val() == "BLIND EYE CAVE FISH")){
+  } if (($("#txtQ5").val() == "BLIND EYE CAVE FISH" || "blind eye cave fish" || "Blind Eye Cave Fish" || "Blind eye cave fish")){
     check=5;
     console.log(check);
-  } if (($("#txtQ6").val() == "BRAIN CORAL")){
+  } if (($("#txtQ6").val() == "BRAIN CORAL" || "brain coral" || "Brain Coral" || "Brain coral")){
     check=6;
     console.log(check);
   }
